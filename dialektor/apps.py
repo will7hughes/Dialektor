022e0e11a55fb5abd876e536e1a624c8e83ac25f
will7hughes <will7hughes@gmail.com>
2021-02-02 21:42:55 -0600

    Delete all

--- dialektor/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DialektorConfig(AppConfig):
-    name = 'dialektor'
--- dialektor/urls.py
+++
@@ -1,8 +0,0 @@
-# users/urls.py
-from django.urls import path
-from .views import SignUpView, logout_view
-
-urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
-
-]
